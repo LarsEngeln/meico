c4d6a37bcc4fab2c8656797a76c05a0a4ced320b
Lars Engeln <mail@lars-engeln.de>
2026-05-11 15:18:06 +0200

    adds guard that in an exported midi files no negative date occur, as this could result with an ornament at the very beginning, which may lead to a midi files that cannot be interpreted by other software

--- src/meico/msm/Msm.java
+++ src/meico/msm/Msm.java
@@ -1249,16 +1249,19 @@ public class Msm extends AbstractMsm {
 
                 Attribute velocityAtt = Helper.getAttribute("velocity", n);                                         // get the velocity attribute
                 int velocity = (velocityAtt == null) ? 100 : Math.round(Float.parseFloat(velocityAtt.getValue()));  // if there is no velocity attribute set velocity to 100 by default, otherwise Math.round(float) outputs the integer velocity
-                track.add(EventMaker.createNoteOn(chan, date, pitch, velocity));
+                track.add(EventMaker.createNoteOn(chan, Math.max(0, date), pitch, velocity));
 
                 long dateEnd;
                 Attribute endAtt = Helper.getAttribute("milliseconds.date.end", n);
                 if (endAtt == null) {
                     System.err.println("Missing attribute \"milliseconds.date.end\" in element " + n.toXML() + ". Using attribute \"duration\" instead.");
                     long dur = Math.round(Double.parseDouble(Helper.getAttributeValue("duration", n)));
-                    dateEnd = date + dur;
+                    dateEnd = Math.max(0, date) + dur;
                 } else {
                     dateEnd = Math.round(Double.parseDouble(endAtt.getValue()));
+                    if(dateEnd < 0) {
+                        dateEnd = -(date-dateEnd);
+                    }
                 }
                 track.add(EventMaker.createNoteOff(chan, dateEnd, pitch, 0));
             } else {
